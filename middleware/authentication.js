07e8aacc827487b78211309cb7a641aa155a45d0
Marco Jahn <marco.jahn@gmail.com>
2014-04-14 09:43:22 +0200

    authentication middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,8 +1,4 @@
-/*
-var auth = function (req, res, next) {
-    console.log('auth middleware');
-    next();
-};*/
+
 exports.auth = function (req, res, next) {
     if (req.originalUrl.indexOf('/anonymous') === 0) {
         console.log('auth middleware :: unsecured service');
