e5ba2923a21f896319274f83c38f4835bcd930a0
Marco Jahn <marco.jahn@gmail.com>
2014-04-15 17:38:57 +0200

    implementing session based authentication

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,9 +1,14 @@
-
 exports.auth = function (req, res, next) {
     if (req.originalUrl.indexOf('/anonymous') === 0) {
         console.log('auth middleware :: unsecured service');
+        next();
     } else {
         console.log('auth middleware :: secured service');
+        if (req.session && req.session.user) {
+            console.log('user is authenticated');
+            next();
+        } else {
+            res.json(666, {success: false, reason: 'not_authenticated'}); // TODO
+        }
     }
-    next();
 };
