8f04671246e548f4a8e8f4af713527e21a2ca2b5
Marco Jahn <marco.jahn@gmail.com>
2014-04-15 18:30:12 +0200

    authorization middleware

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,4 +1,21 @@
-exports.isOwner = function (req, res, next, id) {
+// 'use strict';
+
+module.exports = Roles;
+
+function Roles (options) {
+    options = options || {};
+    this.functionList = [];
+    this.failureHandler = options.failureHandler || defaultFailureHandler;
+    this.async = options.async || false;
+    this.userProperty = options.userProperty || 'user';
+}
+
+function defaultFailureHandler(req, res, action) {
+    res.json(403, {success: false, reason: 'not_authorized'}); // TODO
+}
+
+
+/*exports.isOwner = function (req, res, next, id) {
     // TODO
     console.log('authorization middleware');
     if (!req.session || !req.session.user) {
@@ -12,4 +29,4 @@ exports.isOwner = function (req, res, next, id) {
 
     //next(new Error('Method not allowed'));
     res.json(666, 'You are not allowed to edit that entry');
-};
+};*/
