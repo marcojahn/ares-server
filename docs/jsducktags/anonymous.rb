cae010181abacec7146edb2e951c36e2fe290bda
Marco Jahn <marco.jahn@gmail.com>
2014-04-23 15:41:01 +0200

    Colorized output

--- docs/jsducktags/anonymous.rb
+++ docs/jsducktags/anonymous.rb
@@ -3,12 +3,26 @@ require "jsduck/tag/boolean_tag"
 class Anonymous < JsDuck::Tag::BooleanTag
   def initialize
     @pattern = "anonymous"
-    @signature = {:long => "anonymous", :short => "anon"}
+    @signature = {:long => "Anonymous", :short => "anon"}
     @html_position = POS_DOC + 0.1
+    @css = <<-EOCSS
+      .signature .anonymous {
+        color: orange;
+        background: transparent;
+        border: 1px solid orange;
+      }
+      .inner-box {
+        border-color: orange;
+      }
+    EOCSS
     super
   end
 
   def to_html(context)
-    "<p>This is an anonymous service that can be called without authentication.</p>"
+    <<-EOHTML
+      <div class='rounded-box inner-box'>
+      <p>This is an anonymous service that can be called without authentication.</p>
+      </div>
+    EOHTML
   end
 end
