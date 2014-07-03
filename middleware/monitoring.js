7265cdc6d6d15a32bc5a26a399cf815d5cf672b8
Marco Jahn <marco.jahn@gmail.com>
2014-07-03 15:37:10 +0200

    monitoring changes

--- middleware/monitoring.js
+++ middleware/monitoring.js
@@ -16,13 +16,12 @@ module.exports = function (logger, options) {
         req.kvLog = {
             created: req._rlStartTime.toISOString(),
             method: req.method,
-            url: url.parse(req.originalUrl).pathname,
+            url: '',
             parameter: null,
             status: null,
             duration: null,
             reason: null,
             _rlLevel: 'info' // internal usage
-            // type: 'reqlog'
         };
 
         // Proxy the real end function
@@ -31,31 +30,11 @@ module.exports = function (logger, options) {
             res.end = rEnd;
             res.end(chunk, encoding);
 
-            // And do the work we want now (logging!)
-            // Conditionally do so if url matches a specific exclude.
-            //if (options && options.excludes && options.excludes.indexOf(req.kvLog.url) > -1) return;
-            if (req.kvLog.url.indexOf('/monitoring') > -1) return;
-
             // Save a few more variables that we can only get at the end
             req.kvLog.status = res.statusCode;
             req.kvLog.duration = (new Date() - req._rlStartTime);
             req.kvLog.parameter = req.params || null;
-
-            // Send the log off to winston
-            /*var level = req.kvLog._rlLevel
-             , msg   = req.kvLog.message || '';
-             delete req.kvLog._rlLevel;
-             if (msg.length) {
-             delete req.kvLog.message;
-             }
-             logger.log(level, msg, req.kvLog);*/
-
-            // modify pathname if necessary
-            var pathname = url.parse(req.originalUrl).pathname;
-
-            if (req.params && req.params.id && pathname.substring(pathname.length ,pathname.lastIndexOf("/") + 1) === req.params.id ) {
-                req.kvLog.url = pathname.substring(0 ,pathname.lastIndexOf("/"));
-            }
+            req.kvLog.url = req.baseUrl + req.route.path;
 
             if (res.statusCode !== 200) {
                 var reason = chunk;
